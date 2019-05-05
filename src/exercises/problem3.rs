420d946839ada30c2369213915a58a4de610490b
Jim Hessin <jhessin@gmail.com>
2019-05-05 14:48:30 -0600

    Formatted comments. Added department listing feature, as well as per department listing.

--- src/exercises/problem3.rs
+++ src/exercises/problem3.rs
@@ -1,4 +1,9 @@
-/* Using a hash map and vectors, create a text interface to allow a user to add employee names to a department in a company. For example, “Add Sally to Engineering” or “Add Amir to Sales.” Then let the user retrieve a list of all people in a department or all people in the company by department, sorted alphabetically.
+/* Using a hash map and vectors,
+create a text interface to allow a user to add employee names
+to a department in a company. For example, “Add Sally to Engineering”
+or “Add Amir to Sales.” Then let the user retrieve a list of all people
+in a department or all people in the company by department,
+sorted alphabetically.
 */
 
 use std::collections::HashMap;
@@ -23,6 +28,24 @@ impl Company {
     let entry = self.data.entry(department).or_insert(vec![]);
     entry.push(name);
   }
+
+  fn dept(&self, department: &str) -> Company{
+    let mut new_company = Company::new();
+    if let Some(employees) = self.data.get(department) {
+      for emp in employees {
+        new_company.add(department, emp);
+      }
+    }
+    new_company
+  }
+
+  fn departments(&self) -> Vec<String> {
+    let mut depts = vec![];
+    for (k,v) in &self.data {
+      depts.push(k.clone());
+    }
+    depts
+  }
 }
 
 impl Display for Company {
@@ -43,7 +66,7 @@ pub fn main() {
   let mut company = Company::new();
 
   loop {
-    println!("Please enter a comand: (Add), (Display), (Quit)");
+    println!("Please enter a comand: (Add), (Display), (Departments), (Quit)");
 
     let mut input = String::new();
     io::stdin()
@@ -60,7 +83,15 @@ pub fn main() {
           }
         }
       } else if cmd.eq_ignore_ascii_case("display") {
-        println!("{}", company);
+        if let Some(department) = iter.next() {
+          println!("{}", company.dept(department));
+        } else {
+          println!("{}", company);
+        }
+      } else if cmd.eq_ignore_ascii_case("departments") {
+        for dept in company.departments() {
+            println!("{}", dept);
+        }
       } else if cmd.eq_ignore_ascii_case("quit") {
         println!("Goodbye");
         break;
