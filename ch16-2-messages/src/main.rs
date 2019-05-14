055a7993f950c316d3f3102f7e1fe6447ed56133
Jim Hessin <jhessin@gmail.com>
2019-05-14 16:12:54 -0600

    Finished ch16-2

--- ch16-2-messages/src/main.rs
+++ ch16-2-messages/src/main.rs
@@ -1,13 +1,41 @@
 use std::sync::mpsc;
 use std::thread;
+use std::time::Duration;
 
 fn main() {
     let (tx, rx) = mpsc::channel();
+
+    let tx2 = mpsc::Sender::clone(&tx);
+
     thread::spawn(move || {
-        let val = "hi".to_owned();
-        tx.send(val).unwrap();
+        let vals = vec![
+            "hi".to_owned(),
+            "from".to_owned(),
+            "the".to_owned(),
+            "thread".to_owned(),
+        ];
+
+        for val in vals {
+            tx2.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
     });
 
-    let received = rx.recv().unwrap();
-    println!("Got: {}", received);
+    thread::spawn(move || {
+        let vals = vec![
+            "more".to_owned(),
+            "messages".to_owned(),
+            "for".to_owned(),
+            "you".to_owned(),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
 }
