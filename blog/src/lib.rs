525d63760f6283b5fd294071d5def3ab4b85afc1
Jim Hessin <jhessin@gmail.com>
2019-05-15 11:42:40 -0600

    Finished ch17

--- blog/src/lib.rs
+++ blog/src/lib.rs
@@ -1,23 +1,28 @@
 pub struct Post {
-    state: Option<Box<dyn State>>,
+    content: String,
+}
+
+pub struct DraftPost {
+    content: String,
+}
+
+pub struct PendingReviewPost {
     content: String,
 }
 
 impl Post {
-    pub fn new() -> Post {
-        Post {
-            state: Some(Box::new(Draft {})),
+    pub fn new() -> DraftPost {
+        DraftPost {
             content: String::new(),
         }
     }
 
     pub fn content(&self) -> &str {
-        self.state
-            .as_ref()
-            .unwrap()
-            .content(&self)
+        &self.content
     }
+}
 
+impl DraftPost {
     pub fn add_text(
         &mut self,
         text: &str,
@@ -25,102 +30,19 @@ impl Post {
         self.content.push_str(text);
     }
 
-    pub fn request_review(&mut self) {
-        if let Some(s) = self.state.take() {
-            self.state = Some(s.request_review())
-        }
-    }
-
-    pub fn approve(&mut self) {
-        if let Some(s) = self.state.take() {
-            self.state = Some(s.approve())
+    pub fn request_review(
+        self
+    ) -> PendingReviewPost {
+        PendingReviewPost {
+            content: self.content,
         }
     }
 }
 
-trait State {
-    fn request_review(
-        self: Box<Self>
-    ) -> Box<dyn State>;
-
-    fn approve(self: Box<Self>)
-        -> Box<dyn State>;
-
-    fn content<'a>(
-        &self,
-        _post: &'a Post,
-    ) -> &'a str {
-        ""
-    }
-}
-
-struct Draft {}
-
-impl State for Draft {
-    fn request_review(
-        self: Box<Self>
-    ) -> Box<dyn State> {
-        Box::new(PendingReview {})
-    }
-
-    fn approve(self: Box<Self>) -> Box<State> {
-        self
-    }
-}
-
-struct PendingReview {}
-
-impl State for PendingReview {
-    fn request_review(
-        self: Box<Self>
-    ) -> Box<dyn State> {
-        self
-    }
-
-    fn approve(self: Box<Self>) -> Box<State> {
-        Box::new(Published {})
-    }
-}
-
-pub struct Published {}
-
-impl State for Published {
-    fn request_review(
-        self: Box<Self>
-    ) -> Box<State> {
-        self
-    }
-
-    fn approve(self: Box<Self>) -> Box<State> {
-        self
-    }
-
-    fn content<'a>(
-        &self,
-        post: &'a Post,
-    ) -> &'a str {
-        &post.content
-    }
-}
-#[cfg(test)]
-mod tests {
-    use super::*;
-    #[test]
-    fn it_works() {
-        let mut post = Post::new();
-
-        post.add_text(
-            "I ate a salad for lunch today",
-        );
-        assert_eq!("", post.content());
-
-        post.request_review();
-        assert_eq!("", post.content());
-
-        post.approve();
-        assert_eq!(
-            "I ate a salad for lunch today",
-            post.content()
-        );
+impl PendingReviewPost {
+    pub fn approve(self) -> Post {
+        Post {
+            content: self.content,
+        }
     }
 }
--- ch17-3/src/main.rs
+++ ch17-3/src/main.rs
@@ -1,17 +1,32 @@
-use blog::Post;
+use blog::*;
 
-fn main() {
+fn setup() -> DraftPost {
     let mut post = Post::new();
 
     post.add_text(
         "I ate a salad for lunch today",
     );
-    assert_eq!("", post.content());
 
+    post
+}
+
+#[test]
+fn draft_can_be_submitted_for_review() {
+    let post = setup();
     post.request_review();
-    assert_eq!("", post.content());
+}
 
+#[test]
+fn review_post_can_be_approved() {
+    let post = setup().request_review();
     post.approve();
+}
+
+#[test]
+fn approved_post_has_matching_content() {
+    let post = setup();
+    let post = post.request_review();
+    let post = post.approve();
     assert_eq!(
         "I ate a salad for lunch today",
         post.content()
